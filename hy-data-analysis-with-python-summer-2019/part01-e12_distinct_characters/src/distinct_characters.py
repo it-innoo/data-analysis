9df043b605f2a28619c5a3404ac1866e18d5da8b
jailgreen <36865973+jailgreen@users.noreply.github.com>
2019-08-30 14:27:34 +0300

    part01-e12

--- hy-data-analysis-with-python-summer-2019/part01-e12_distinct_characters/src/distinct_characters.py
+++ hy-data-analysis-with-python-summer-2019/part01-e12_distinct_characters/src/distinct_characters.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 
+
 def distinct_characters(L):
-    return {}
+    d = {}
+    for l in L:
+        s = l
+        d[s] = len(set(s))
+
+    return d
+
 
 def main():
     print(distinct_characters(["check", "look", "try", "pop"]))
+
 
 if __name__ == "__main__":
     main()
