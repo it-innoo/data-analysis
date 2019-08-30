4d394c01b69d1d8cb992c66811e2217f676ac119
jailgreen <36865973+jailgreen@users.noreply.github.com>
2019-08-30 12:21:56 +0300

    part01-e11

--- hy-data-analysis-with-python-summer-2019/part01-e11_interleave/src/interleave.py
+++ hy-data-analysis-with-python-summer-2019/part01-e11_interleave/src/interleave.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 
+import itertools
+
+
 def interleave(*lists):
-    return []
+    L = zip(*lists)
+    return list(itertools.chain(*L))
+
 
 def main():
     print(interleave([1, 2, 3], [20, 30, 40], ['a', 'b', 'c']))
+
 
 if __name__ == "__main__":
     main()
