8d56040caf22a623cbb1ec925fbe4340554b3f4f
jailgreen <36865973+jailgreen@users.noreply.github.com>
2019-09-02 13:10:56 +0300

    part01-e13

--- hy-data-analysis-with-python-summer-2019/part01-e13_reverse_dictionary/src/reverse_dictionary.py
+++ hy-data-analysis-with-python-summer-2019/part01-e13_reverse_dictionary/src/reverse_dictionary.py
@@ -1,10 +1,31 @@
 #!/usr/bin/env python3
 
+
 def reverse_dictionary(d):
-    return {}
+    inv = {}
+
+    for key in d:
+        for value in d[key]:
+            if value in inv.keys():
+                values = inv.get(value)
+                values.append(key)
+            else:
+                inv[value] = [key]
+    return inv
+
 
 def main():
-    pass
+    d = {
+        'move': ['liikuttaa'],
+        'hide': ['piilottaa', 'salata'],
+        'six': ['kuusi'],
+        'fir': ['kuusi']
+    }
+    print(d)
+    inv = reverse_dictionary(d)
+    print("===")
+    print(inv)
+
 
 if __name__ == "__main__":
     main()
