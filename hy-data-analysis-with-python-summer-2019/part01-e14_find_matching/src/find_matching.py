3c401878320a831583c20842e404d872d6845fee
jailgreen <36865973+jailgreen@users.noreply.github.com>
2019-09-02 13:43:10 +0300

    part01-e14

--- hy-data-analysis-with-python-summer-2019/part01-e14_find_matching/src/find_matching.py
+++ hy-data-analysis-with-python-summer-2019/part01-e14_find_matching/src/find_matching.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python3
 
+
 def find_matching(L, pattern):
-    return []
+    R = []
+
+    for i, x in enumerate(L):
+        if pattern in x:
+            R.append(i)
+
+    return R
+
 
 def main():
-    pass
+    m = find_matching(["sensitive", "engine", "rubbish", "comment"], "en")
+    print(m)
+
 
 if __name__ == "__main__":
     main()
