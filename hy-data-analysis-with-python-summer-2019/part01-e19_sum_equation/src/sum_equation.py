0462fb796034cf8f746802374e29e76a2574e086
jailgreen <36865973+jailgreen@users.noreply.github.com>
2019-09-03 11:48:55 +0300

    part01-e19

--- hy-data-analysis-with-python-summer-2019/part01-e19_sum_equation/src/sum_equation.py
+++ hy-data-analysis-with-python-summer-2019/part01-e19_sum_equation/src/sum_equation.py
@@ -1,10 +1,33 @@
 #!/usr/bin/env python3
 
+
 def sum_equation(L):
-    return ""
+    if not L:
+        return "0 = 0"
+
+    result = list(map(str, L))
+    return " + ".join(result) + f" = {sum(L)}"
+
+    # My tryout version
+    if len(L) == 0:
+        return "0 = 0"
+
+    from functools import reduce
+    summa = reduce(lambda x, y: x+y, L, 0)
+    S = []
+
+    for i in L:
+        S.append(str(i))
+
+    result = " + ".join(S)
+    result += " = " + str(summa)
+
+    return result
+
 
 def main():
-    pass
+    print(sum_equation([1, 5, 7]))
+
 
 if __name__ == "__main__":
     main()
